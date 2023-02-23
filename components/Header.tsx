d414093a9a6c4ee092e196032158fba8086858aa
Dmitry <punkreatine@gmail.com>
2023-02-24 01:02:12 +0300

    feat: new style

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,14 +1,31 @@
 import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClapperboard, faEarth, faHouse, faQuestion, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
     <header className="fixed px-3 w-full flex justify-between bg-primary text-white text-bold text-sm sm:text-base z-10 sm:font-bold h-12 items-center pointer-events-auto overflow-x-auto">
-      <Link href="/" className='hover:text-green before:content-["#"] before:mr-0.5 sm:mr-0 mr-12 transition ease-in-out delay-75'>Home</Link>
-      <nav className='flex gap-6 mx-4 p-2'>
-        <Link href="/characters" className='hover:text-blue before:content-["/"] before:mr-0.5 transition ease-in-out delay-75'>Characters</Link>
-        <Link href="/locations" className='hover:text-pink before:content-["/"] before:mr-0.5 transition ease-in-out delay-75'>Locations</Link>
-        <Link href="/episodes" className='hover:text-orange before:content-["/"] before:mr-0.5 transition ease-in-out delay-75'>Episodes</Link>
-        <Link href="/survey" className='hover:text-orange before:content-["/"] before:mr-0.5 transition ease-in-out delay-75'>???</Link>
+      <Link href="/" className="hover:text-green sm:mr-0 mr-12 transition ease-in-out delay-75 group">
+        <FontAwesomeIcon icon={faHouse} height="24px" />
+        <span className='tooltip group-hover:scale-100'>Home</span>
+      </Link>
+      <nav className="flex gap-6 mx-4 p-2 items-center static">
+        <Link href="/characters" className="hover:text-blue touch-auto transition ease-in-out delay-75 group">
+          <FontAwesomeIcon icon={faUsers} height="24px" />
+          <span className="tooltip group-hover:scale-100">Characters</span>
+        </Link>
+        <Link href="/locations" className="hover:text-pink transition ease-in-out delay-75 group">
+          <FontAwesomeIcon icon={faEarth} height="24px" />
+          <span className="tooltip group-hover:scale-100">Locations</span>
+        </Link>
+        <Link href="/episodes" className="hover:text-orange transition ease-in-out delay-75 group">
+          <FontAwesomeIcon icon={faClapperboard} height="24px" />
+          <span className="tooltip group-hover:scale-100">Episodes</span>
+        </Link>
+        <Link href="/survey" className="hover:text-pink transition ease-in-out delay-75 group">
+          <FontAwesomeIcon icon={faQuestion} height="24px" />
+          <span className='tooltip group-hover:scale-100'>Survey</span>
+        </Link>
       </nav>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
+
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    colors: {
-      'green': '#CDF567',
-      'pink': '#FFD0D5',
-      'orange': '#ffc68e',
-      'blue': '#A4C9D8',
-      'primary': '#171313',
-      'yellow': '#ffe818',
-      'gray': '#837e7e',
-      'white': '#ffffff',
-      'red': '#ff0101'
-    },
     extend: {
+      colors: {
+        'green': '#CDF567',
+        'pink': '#FFD0D5',
+        'orange': '#ffc68e',
+        'blue': '#A4C9D8',
+        'primary': '#171313',
+        'yellow': '#ffe818',
+        'gray': '#837e7e',
+        'white': '#ffffff',
+        'red': '#ff0101'
+      },
     },
   },
   plugins: [],
