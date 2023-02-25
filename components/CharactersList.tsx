2077734291672590a500a7f2565d7614d2029b48
Dmitry <punkreatine@gmail.com>
2023-02-25 12:52:00 +0300

    fix: charactersList

--- components/CharactersList.tsx
+++ components/CharactersList.tsx
@@ -8,41 +8,40 @@ type Props = {
   data: Character[];
 };
 
-export function CharactersList({ data }: Props) { 
+export function CharactersList({ data }: Props) {
   const statusStyle = (status: 'Dead' | 'Alive' | 'unknown', style: string) =>
-    clsx(
-      style,
-      status == 'unknown' && 'bg-gray',
-      status === 'Dead' && 'bg-red',
-      status === 'Alive' && 'bg-green',
-    );
+    clsx(style, status == 'unknown' && 'bg-gray', status === 'Dead' && 'bg-red', status === 'Alive' && 'bg-green');
 
   return (
     <div className="flex flex-wrap justify-center mt-8">
       {data.map((elem) => (
         <article key={elem.id} className="bg-primary  text-white mt-4 mx-0 h-60 sm:w-[500px] sm:m-2 w-11/12 rounded-2xl flex ">
-          <div className="flex flex-[3_1_0%] w-full rounded-2xl">
-            <Image
-              src={elem.image}
-              alt={elem.name}
-              width={1000}
-              height={1000}
-              className="w-full h-full object-cover object-center rounded-l-xl"
-            />
-          </div>
-          <div className="flex flex-[3_1_0%] pt-4 flex-col relative bg-primary rounded-r-xl">
-            <Link href={elem.url}>
-              <p className="font-semibold text-xl pb-6 px-6">{elem.name}</p>
-            </Link>
-            <span className="px-6">
-              Status: {elem.status}
-              <span className="h-3 w-3 left-2 relative">
-                <span className={statusStyle(elem.status, 'animate-ping inline-flex absolute top-1 h-3 w-3 rounded-full')}></span>
-                <span className={statusStyle(elem.status, 'absolute top-1 inline-flex rounded-full h-3 w-3 opacity-40')}></span>
+          <div className="flex flex-row w-full rounded-2xl">
+            <div className="basis-1/2">
+              <Image
+                src={elem.image}
+                alt={elem.name}
+                width={1000}
+                height={1000}
+                className="w-full basis-1/2 h-full object-cover object-center rounded-l-xl"
+              />
+            </div>
+            <div className="flex basis-1/2 pt-4 flex-col relative bg-primary rounded-r-xl pl-4 pr-2">
+              <Link href={elem.url}>
+                <p className="font-semibold text-xl pb-6">{elem.name}</p>
+              </Link>
+              <span>
+                Status: {elem.status}
+                <span className="h-3 w-3 left-2 relative">
+                  <span
+                    className={statusStyle(elem.status, 'animate-ping inline-flex absolute top-1 h-3 w-3 rounded-full')}
+                  ></span>
+                  <span className={statusStyle(elem.status, 'absolute top-1 inline-flex rounded-full h-3 w-3 opacity-40')}></span>
+                </span>
               </span>
-            </span>
-            <p className="pl-6">Gender: {elem.gender}</p>
-            <p className="pl-6">Species: {elem.species}</p>
+              <p>Gender: {elem.gender}</p>
+              <p>Species: {elem.species}</p>
+            </div>
           </div>
         </article>
       ))}
